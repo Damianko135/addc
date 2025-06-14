8684f74216665d220bfc6bdca4c1a1a8dfdf532c
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2025-06-15 00:25:51 +0200

    trying something different

--- public/app.js
+++
@@ -1,9 +0,0 @@
-// // Unless specified, it will think it is dev... Should specify it somewhere in the build process
-// const environment = (window?.ENV || 'development'); // safer if injected at runtime
-
-//   const link = document.createElement('link');
-//   link.rel = 'stylesheet';
-//   link.href = environment === 'development' 
-//       ? 'style.css?v=' + new Date().getTime() // Always fresh
-//       : 'style.css'; // Cached in production
-//   document.head.appendChild(link);
